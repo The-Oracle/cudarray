ab05730e13ff54ea95616b159e889902a8af3051
Jeremy Brown <Jeremy@jbdynamics.net>
2018-12-10 18:30:05 -0700

    Changed cudNN Convolution2dDescriptor to FP

--- src/nnet/cudnn.cpp
+++ src/nnet/cudnn.cpp
@@ -165,7 +165,7 @@ void ConvBC01CuDNN<T>::fprop(const T *imgs, const T *filters, int n_imgs,
   }
   if (set_conv_desc) {
     CUDNN_CHECK(cudnnSetConvolution2dDescriptor(
-        conv_desc, pad_y, pad_x, stride_y, stride_x, 1, 1, CUDNN_CONVOLUTION, CUDNN_DATA_INT32
+        conv_desc, pad_y, pad_x, stride_y, stride_x, 1, 1, CUDNN_CONVOLUTION, CUDNN_DATA_FLOAT
     ));
     int n, c, h, w;
     CUDNN_CHECK(cudnnGetConvolution2dForwardOutputDim(
